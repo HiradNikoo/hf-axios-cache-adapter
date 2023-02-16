5ec8681adc6c9ed6e7f0d0e1986269dba1616b87
rafaelmrdyn <rafaelmrdyn@gmail.com>
2023-02-16 16:19:59 +0400

    fix redisstore

--- src/redis.js
+++ src/redis.js
@@ -1,54 +1,50 @@
 import { promisify } from 'util'
 
-import { mapObject } from './utilities'
+const isObject = (value) => {
+  const type = typeof value
+  return value != null && (type === 'object' || type === 'function')
+}
 
+const mapObject = (value, fn) => {
+  if (!isObject(value)) return []
+  return Object.keys(value).map(key => fn(value[key], key))
+}
 class RedisStore {
-  constructor (client, HASH_KEY = 'axios-cache') {
-    const invalidClientError = new TypeError(
-      'First parameter must be a valid RedisClient instance.'
-    )
+  constructor(client, HASH_KEY = 'axios-cache') {
 
-    try {
-      if (client.constructor.name !== 'RedisClient') {
-        throw invalidClientError
-      }
-    } catch (err) {
-      throw invalidClientError
-    }
     this.client = client
     this.HASH_KEY = HASH_KEY
-    this.hgetAsync = promisify(client.hget).bind(client)
-    this.hsetAsync = promisify(client.hset).bind(client)
-    this.hdelAsync = promisify(client.hdel).bind(client)
-    this.delAsync = promisify(client.del).bind(client)
-    this.hlenAsync = promisify(client.hlen).bind(client)
-    this.hgetallAsync = promisify(client.hgetall).bind(client)
+    this.hgetAsync = promisify(client.HGET).bind(client)
+    this.hsetAsync = promisify(client.HSET).bind(client)
+    this.hdelAsync = promisify(client.HDEL).bind(client)
+    this.delAsync = promisify(client.DEL).bind(client)
+    this.hlenAsync = promisify(client.HLEN).bind(client)
+    this.hgetallAsync = promisify(client.HGETALL).bind(client)
   }
 
-  async getItem (key) {
+  async getItem(key) {
     const item = (await this.hgetAsync(this.HASH_KEY, key)) || null
-
     return JSON.parse(item)
   }
 
-  async setItem (key, value) {
+  async setItem(key, value) {
     await this.hsetAsync(this.HASH_KEY, key, JSON.stringify(value))
     return value
   }
 
-  async removeItem (key) {
+  async removeItem(key) {
     await this.hdelAsync(this.HASH_KEY, key)
   }
 
-  async clear () {
+  async clear() {
     await this.delAsync(this.HASH_KEY)
   }
 
-  async length () {
+  async length() {
     return this.hlenAsync(this.HASH_KEY)
   }
 
-  async iterate (fn) {
+  async iterate(fn) {
     const hashData = await this.hgetallAsync(this.HASH_KEY)
     return Promise.all(mapObject(hashData, fn))
   }
